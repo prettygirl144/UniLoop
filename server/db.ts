59dec97e5be7abb329e79d6f7171866c4e3f255b
prettygirl144 <102664675+prettygirl144@users.noreply.github.com>
2025-12-01 03:05:12 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,8 @@
+import pkg from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
 import * as schema from "@shared/schema";
+
+const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
